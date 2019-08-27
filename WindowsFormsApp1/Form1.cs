0f2f33753ea6293cf46a96d51083a5681fc0e73e
Jhusseth <jhusseth.sanchez@correo.icesi.edu.co>
2019-08-27 17:10:08 -0500

    update

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,15 +16,18 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         String directionFolder;
-        String directionFile;
+       
         private Random random;
-        private static int RUN = 10;
+        private static int RUN = 32;
         private static int[] dataArray;
-        private static int EXPERIMENT = 100;
+        private static int EXPERIMENT = 20;
+        private static int sizeArrayOne = 1048576;
+        private static int sizeArrayTwo = 65536;
+        private static int sizeArrayThree = 131072;
         string root;
         string timeRoot;
         string[] directions;
-        Clock clk;
+        
 
         public Form1()
         {
@@ -56,30 +60,27 @@ namespace WindowsFormsApp1
         {
             random = new Random();
             directions = new string[3];
-            generateArrayInfo(1000,0);
-            generateArrayInfo(40,1);
-            generateArrayInfo(80,2);
+            generateArrayInfo(sizeArrayOne, 0);
+            generateArrayInfo(sizeArrayTwo, 1);
+            generateArrayInfo(sizeArrayThree, 2);
         }
 
-        public void generateArrayInfo(int size,int pos)
+        public void generateArrayInfo(int size, int pos)
         {
             StringBuilder cvsContent = new StringBuilder();
             string aux = "";
 
-            for (int i = 0; i < size-1; i++)
+            for (int i = 0; i < size - 1; i++)
             {
-                if (i == 0)
-                {
-                    aux = "" + random.Next(4000);
-                }
-                aux += "," + random.Next(4000);
 
-               
+                aux = "" + random.Next(4000);
 
-                
+                cvsContent.AppendLine(aux);
+
+
 
             }
-            cvsContent.AppendLine(aux);
+
             directions[pos] = directionFolder + "/FileSize" + size + ".csv";
             File.AppendAllText(directions[pos], cvsContent.ToString());
         }
@@ -108,24 +109,24 @@ namespace WindowsFormsApp1
 
                 transformInfoInArray(2);
                 timSort(ref dataArray, dataArray.Length);
-                writeOrderArray(1);
+               writeOrderArray(1);
                 transformInfoInArray(2);
                 timeExperiment(2,1);
 
                 transformInfoInArray(0);
-                timSort(ref dataArray, dataArray.Length);
+                mergeSort(ref dataArray, 0, dataArray.Length-1);
                 writeOrderArray(2);
                 transformInfoInArray(0);
                 timeExperiment(0,2);
 
                 transformInfoInArray(1);
-                timSort(ref dataArray, dataArray.Length);
-                writeOrderArray(2);
+               mergeSort(ref dataArray,0, dataArray.Length-1);
+               writeOrderArray(2);
                 transformInfoInArray(1);
                 timeExperiment(1,2);
 
                 transformInfoInArray(2);
-                timSort(ref dataArray, dataArray.Length);
+                mergeSort(ref dataArray, 0,dataArray.Length-1);
                 writeOrderArray(2);
                 transformInfoInArray(2);
                 timeExperiment(2,2);
@@ -163,16 +164,20 @@ namespace WindowsFormsApp1
                     
                     sw.Start();
                     timSort(ref dataArray, dataArray.Length);
+                  
+                    
+
                     sw.Stop();
+                    
                     transformInfoInArray(pos);
-                    aux = "" + (sw.Elapsed.TotalMilliseconds * 1000000);
+                    aux = "" + (sw.Elapsed.TotalMilliseconds);
                     cvsContent.AppendLine(aux);
 
                 }
             }
             else if (proc == 2)
             {
-               
+                string aux2 = "";
                 auxRoot = timeRoot + "/MergeSort";
                 if (!Directory.Exists(auxRoot))
                 {
@@ -180,16 +185,20 @@ namespace WindowsFormsApp1
                 }
                 for (int i = 1; i < EXPERIMENT; i++)
                 {
-                    Stopwatch sw = new Stopwatch();
+                    Stopwatch sws = new Stopwatch();
 
 
-                    long initTime = nanoTime();
-                    //sw.Start();
-                    mergeSort(ref dataArray, dataArray.Length,0);
-                    //sw.Stop();
-                    aux = "" + (nanoTime() - initTime);
-                    Console.WriteLine(aux);
+                    
+                    sws.Start();
+                    mergeSort(ref dataArray, dataArray.Length, 0);
+                   
+                    
+
+                    sws.Stop();
                     transformInfoInArray(pos);
+                    aux = "" + (sws.Elapsed.TotalMilliseconds);
+                    
+                 
                     
                     cvsContent.AppendLine(aux);
 
@@ -200,13 +209,7 @@ namespace WindowsFormsApp1
 
 
         }
-        private static long nanoTime()
-        {
-            long nano = 10000L * Stopwatch.GetTimestamp();
-            nano /= TimeSpan.TicksPerMillisecond;
-            nano *= 100L;
-            return nano;
-        }
+
         public void writeOrderArray(int al)
         {
             StringBuilder cvsContent = new StringBuilder();
@@ -214,13 +217,12 @@ namespace WindowsFormsApp1
 
             for (int i = 0; i < dataArray.Length - 1; i++)
             {
-                if (i == 0)
-                {
-                    aux = "" + dataArray[i];
-                }
-                aux += "," + dataArray[i];
+
+                aux = ""+dataArray[i];
+
+                cvsContent.AppendLine(aux);
             }
-            cvsContent.AppendLine(aux);
+            
             if(al == 1)
             {
                 string auxiliarDir = root + "/TimsortFileSize" +dataArray.Length  + ".csv";
@@ -237,47 +239,46 @@ namespace WindowsFormsApp1
 
         public int[] transformInfoInArray(int pos)
         {
-            char[] aux = directions[pos].ToCharArray();
-            string par = "";
-            Boolean stop = false;
-            for(int i = aux.Length-5; i> 0 && !stop; i--)
-            {
-                if ((int) aux[i] >= 48 && (int) aux[i] <= 57)
-                {
-                    par += aux[i];
-                }
-                else if((int) aux[i] == 101)
-                {
-                    stop = true;
-                    char[] charArray = par.ToCharArray();
-                    Array.Reverse(charArray);
-                    par = new string(charArray);
-                }
-               
-
-                
-            }
-           dataArray = null;
-            string[] auxArray;
             string line = "";
 
             using (StreamReader file = new StreamReader(directions[pos]))
             {
+                int i = 0;
+                if (pos == 0) dataArray = new int[sizeArrayOne];
+                else if (pos == 1) dataArray = new int[sizeArrayTwo];
+                else if (pos == 2) dataArray = new int[sizeArrayThree]; 
+
                 while ((line = file.ReadLine()) != null)                //Leer linea por linea
                 {
-                    auxArray = line.Split(',');
-                    dataArray = new int[auxArray.Length];
-                    for(int i = 0; i < auxArray.Length; i++)
-                    {
-                       dataArray[i] =  int.Parse(auxArray[i]);
-                    }
-                }
-                }
 
-                return dataArray;
+                    if (pos == 0)
+                    {
+
+
+                        dataArray[i] = int.Parse(line);
+                        i++;
+                    }
+                    if (pos == 1)
+                    {
+
+
+                        dataArray[i] = int.Parse(line);
+                        i++;
+                    }
+                    if (pos == 2)
+                    {
+
+                        dataArray[i] = int.Parse(line);
+                        i++;
+                    }
+
+                }
+            }
+
+            return dataArray;
         }
 
-        public static void insertionSort(int[] arr, int left, int right)
+        public static void insertionSort(ref int[] arr, int left, int right)
         {
             for (int i = left + 1; i <= right; i++)
             {
@@ -346,7 +347,7 @@ namespace WindowsFormsApp1
         {
             // Sort individual subarrays of size RUN  
             for (int i = 0; i < n; i += RUN)
-                insertionSort(arr, i, Math.Min((i + 31), (n - 1)));
+                insertionSort(ref arr, i, Math.Min((i + 31), (n - 1)));
 
             // start merging from size RUN (or 32). It will merge  
             // to form size 64, then 128, 256 and so on ....  
@@ -373,7 +374,7 @@ namespace WindowsFormsApp1
 
         }
 
-        void mergeSort(ref int[] arr, int l, int r)
+        Boolean mergeSort(ref int[] arr, int l, int r)
         {
             if (l < r)
             {
@@ -387,6 +388,7 @@ namespace WindowsFormsApp1
 
                 merge(arr, l, m, r);
             }
+            return true;
         }
     }
 }
